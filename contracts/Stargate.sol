d8d054f44171c550bf16da6b575c92917deea204
zeqing-guo <zqguo92@gmail.com>
2018-08-24 14:04:24 +0800

    modify the callback function

--- contracts/Stargate.sol
+++ contracts/Stargate.sol
@@ -3,36 +3,60 @@ pragma solidity ^0.4.24;
 import 'openzeppelin-solidity/contracts/ownership/Ownable.sol';
 import 'openzeppelin-solidity/contracts/math/SafeMath.sol';
 
-contract Stargate is Ownable {
+import './StargateInterface.sol';
+
+contract Stargate is StargateInterface, Ownable {
 	using SafeMath for uint256;
 
-	mapping (address => uint256) private pledges;
-	mapping (address => uint32) private sequenceIDs;
+	mapping(address => uint256) private pledges;
+	mapping(address => uint32) private sequenceIDs;
+	mapping(address => mapping(uint32 => function(string memory) external)) private requests;
 
-	event Create(address indexed from, uint32 indexed sequenceID, bytes32 indexed databaseID, uint32 nodeCnt, uint256 ethamount);
-	event Delete(address indexed from, uint32 indexed sequenceID, bytes32 indexed databaseID);
-	event Query(address indexed from, uint32 indexed sequenceID, bytes32 indexed databaseID, bytes query);
+	event Create(address from, uint32 sequenceID, uint16 nodeCnt, uint256 ethamount);
+	event Delete(address from, uint32 sequenceID, bytes databaseID);
+	event Query(address from, uint32 sequenceID, bytes databaseID, string query);
+	event Exec(address from, uint32 sequenceID, bytes databaseID, string query);
 
-	// createDB sends a signal to CovenantSQL to create a database with dbID as database id
-	function createDB(uint32 nodeCnt) external payable returns (bytes32 dbID, uint32 seqID) {
+	// create sends a signal to CovenantSQL to create a database with dbID as database id
+	function create(uint16 nodeCnt, function(string memory) external callback) external payable returns (uint32 seqID) {
+		pledges[msg.sender] = pledges[msg.sender].add(msg.value);
 		seqID = sequenceIDs[msg.sender];
 		sequenceIDs[msg.sender] = seqID + 1;
-		dbID = keccak256(this, msg.sender, seqID, nodeCnt);
-		pledges[msg.sender] = pledges[msg.sender].add(msg.value);
-		emit Create(msg.sender, seqID, dbID, nodeCnt, pledges[msg.sender]);
+		mapping(uint32 => function(string memory) external) sequenceId2Callback = requests[msg.sender];
+		sequenceId2Callback[seqID] = callback;
+		emit Create(msg.sender, seqID, nodeCnt, pledges[msg.sender]);
 	}
 
-	// deleteDB sends a signal to CovenantSQL to delete a database whose database id is dbID
-	function deleteDB(bytes32 dbID) external returns (uint32 seqID) {
+	// drop sends a signal to CovenantSQL to drop a database whose database id is dbID
+	function drop(bytes dbID, function(string memory) external callback) external returns (uint32 seqID) {
 		seqID = sequenceIDs[msg.sender];
 		sequenceIDs[msg.sender] = seqID + 1;
+		mapping(uint32 => function(string memory) external) sequenceId2Callback = requests[msg.sender];
+		sequenceId2Callback[seqID] = callback;
 		emit Delete(msg.sender, seqID, dbID);
 	}
 
-	// queryDB sends a signal to CovenantSQL to query on the database whoes id is
-	function queryDB(bytes32 dbID, bytes query) external payable returns (uint32 seqID) {
+	// query sends a signal to CovenantSQL to query a query on the database whoes id is
+	function query(bytes dbID, string _query, function(string memory) external callback) external payable returns (uint32 seqID) {
 		seqID = sequenceIDs[msg.sender];
 		sequenceIDs[msg.sender] = seqID + 1;
-		emit Query(msg.sender, seqID, dbID, query);
+		mapping(uint32 => function(string memory) external) sequenceId2Callback = requests[msg.sender];
+		sequenceId2Callback[seqID] = callback;
+		emit Query(msg.sender, seqID, dbID, _query);
+	}
+
+	// exec sends a signal to CovenantSQL to exec a query on the database whoes id is
+	function exec(bytes dbID, string _query, function(string memory) external callback) external payable returns (uint32 seqID) {
+		seqID = sequenceIDs[msg.sender];
+		sequenceIDs[msg.sender] = seqID + 1;
+		mapping(uint32 => function(string memory) external) sequenceId2Callback = requests[msg.sender];
+		sequenceId2Callback[seqID] = callback;
+		emit Exec(msg.sender, seqID, dbID, _query);
+	}
+
+	// reply receives results from CovenantSQL
+	function reply(address requester, uint32 sequenceID, string response) external onlyOwner {
+		requests[requester][sequenceID](response);
+		delete requests[requester][sequenceID];
 	}
 }
--- truffle-config.js
+++ truffle-config.js
@@ -5,7 +5,8 @@ module.exports = {
     development: {
       host: "127.0.0.1",
       port: 7545,
-      network_id: "*" // match any network
+      network_id: "*", // match any network
+      gas: 6721975,
     }
   }
 };
